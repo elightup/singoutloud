30874172a07cee3c23150eac7b52b3f60048f965
tuyenlq94 <tuyenicolor@gmail.com>
2022-09-16 10:15:05 +0700

    fix font chữ

---
+++ page-templates/register.php
@@ -0,0 +1,16 @@
+<?php // @codingStandardsIgnoreLine
+/**
+ * Template Name: Đăng ký
+ */
+?>
+<?php get_header(); ?>
+
+<main id="primary" class="site-main ">
+
+	<?php
+	get_template_part( 'template-parts/about' );
+	?>
+
+</main>
+
+<?php get_footer(); ?>
