fd95cbcc5916fd168cd66f966c0a7913c250a7e7
ToanNguyen <nguyenthetoan.ntt@gmail.com>
2022-09-19 17:28:15 +0700

    Header fix style

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html class="no-js" <?php language_attributes() ?>>
 
 <head>
-	<meta charset="<?php bloginfo( 'charset' ) ?>">
+	<meta charset="<?php bloginfo('charset') ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,25 +15,61 @@
 
 	<!-- nội dung header -->
 	<div class="header">
+		<div class="header-login">
+			<?php if (!is_user_logged_in()) : ?>
+				<a class="signin " href="<?php echo esc_url(home_url()); ?>/dang-nhap">Đăng nhập</a>
+				<a class="signup" href="<?php echo esc_url(home_url()); ?>/dang-ky">Đăng ký</a>
+			<?php
+			else :
+				$user_current = wp_get_current_user();
+			?>
+				<span>Chào bạn, <?php echo esc_html($user_current->display_name); ?></span>
+			<?php endif ?>
+			<?php if (is_user_logged_in()) : ?>
+				<div class="menu-account d-flex">
+					<a class=" btn-drop-down" href="#popup-logout">
+						<img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/drop-down.png" />
+					</a>
+					<div class="menu-account__wrapper">
+						<ul>
+							<li>
+								<?php SingOutLoud_Icons::render('upload'); ?>
+								<a class="" href="<?php echo esc_html(home_url()) ?>/up-bai-du-thi/">Up bài dự thi</a>
+							</li>
+							<li>
+								<?php SingOutLoud_Icons::render('logout'); ?>
+								<a class="popup-modal" href="#popup-logout">Đăng xuất</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div id="popup-logout" class="popup-logout mfp-hide white-popup-block">
+					<h3>Xin xác nhận</h3>
+					<p>Bạn có muốn chắc đăng xuất.</p>
+					<a class="btn-secondary wp-block-button__link popup-modal-dismiss">Không</a>
+					<a class="btn-primary wp-block-button__link" href="<?= esc_url(wp_logout_url('/')); ?>">Có</a>
+				</div>
+			<?php endif ?>
+		</div>
 		<div class="d-flex space-between">
 			<div class="header__inner space-between">
 				<div class="header__logo">
-					<a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>">
+					<a class="navbar-brand" href="<?php echo esc_url(home_url()); ?>">
 						<?php
-						$logo_white = get_theme_mod( 'singoutloud_logo_white' );
-						$logo_black = get_theme_mod( 'singoutloud_logo_black' );
-						if ( empty( $logo_black ) && empty( $logo_white ) ) :
-							bloginfo( 'name' );
+						$logo_white = get_theme_mod('singoutloud_logo_white');
+						$logo_black = get_theme_mod('singoutloud_logo_black');
+						if (empty($logo_black) && empty($logo_white)) :
+							bloginfo('name');
 						else :
-							?>
-							<img class="navbar-logo" src="<?= esc_url( $logo_black ); ?>" alt="Logo">
-							<img class="navbar-logo white-logo" src="<?= esc_url( $logo_white ); ?>" alt="Logo">
+						?>
+							<img class="navbar-logo" src="<?= esc_url($logo_black); ?>" alt="Logo">
+							<img class="navbar-logo white-logo" src="<?= esc_url($logo_white); ?>" alt="Logo">
 						<?php endif; ?>
 					</a>
 				</div>
 				<div class="header__menu">
 					<?php
-					if ( ! is_user_logged_in() ) :
+					if (!is_user_logged_in()) :
 						wp_nav_menu(
 							array(
 								'theme_location' => 'primary',
@@ -53,57 +89,21 @@
 					?>
 
 					<button class="menu-toggle header-icon" aria-controls="primary-menu" aria-expanded="false">
-						<?php SingOutLoud_Icons::render( 'menu' ) ?>
+						<?php SingOutLoud_Icons::render('menu') ?>
 					</button>
 				</div>
 			</div>
 			<div class="header__btn">
-				<div class="header-login">
-					<?php if ( ! is_user_logged_in() ) : ?>
-						<a class="signin " href="<?php echo esc_url( home_url() ); ?>/dang-nhap">Đăng nhập</a>
-						<a class="signup" href="<?php echo esc_url( home_url() ); ?>/dang-ky">Đăng ký</a>
-						<?php
-					else :
-						$user_current = wp_get_current_user();
-						?>
-						<span>Chào bạn, <?php echo esc_html( $user_current->display_name ); ?></span>
-					<?php endif ?>
-					<?php if ( is_user_logged_in() ) : ?>
-						<div class="menu-account d-flex">
-							<a class=" btn-drop-down" href="#popup-logout">
-								<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/drop-down.png" />
-							</a>
-							<div class="menu-account__wrapper">
-								<ul>
-									<li>
-										<?php SingOutLoud_Icons::render( 'upload' ); ?>
-										<a class="" href="<?php echo esc_html( home_url() ) ?>/up-bai-du-thi/">Up bài dự thi</a>
-									</li>
-									<li>
-										<?php SingOutLoud_Icons::render( 'logout' ); ?>
-										<a class="popup-modal" href="#popup-logout">Đăng xuất</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<div id="popup-logout" class="popup-logout mfp-hide white-popup-block">
-							<h3>Xin xác nhận</h3>
-							<p>Bạn có muốn chắc đăng xuất.</p>
-							<a class="btn-secondary wp-block-button__link popup-modal-dismiss">Không</a>
-							<a class="btn-primary wp-block-button__link" href="<?= esc_url( wp_logout_url( '/' ) ); ?>">Có</a>
-						</div>
-					<?php endif ?>
-				</div>
 				<div class="header-email">
 					<?php
-					$email = get_theme_mod( 'singoutloud_custom_email' );
-					$phone = get_theme_mod( 'singoutloud_custom_phone' );
-					if ( empty( $email ) && empty( $phone ) ) :
+					$email = get_theme_mod('singoutloud_custom_email');
+					$phone = get_theme_mod('singoutloud_custom_phone');
+					if (empty($email) && empty($phone)) :
 						return;
 					else :
-						?>
-						<a href="mailto:<?= esc_attr( $email ); ?>"><?php SingOutLoud_Icons::render( 'email' ); ?> <?= esc_html( $email ); ?></a>
-						<a href="tel:<?= esc_attr( $phone ); ?>"><?php SingOutLoud_Icons::render( 'phone' ); ?> <?= esc_html( $phone ); ?></a>
+					?>
+						<a href="mailto:<?= esc_attr($email); ?>"><?php SingOutLoud_Icons::render('email'); ?> <?= esc_html($email); ?></a>
+						<a href="tel:<?= esc_attr($phone); ?>"><?php SingOutLoud_Icons::render('phone'); ?> <?= esc_html($phone); ?></a>
 					<?php endif; ?>
 				</div>
 			</div>
